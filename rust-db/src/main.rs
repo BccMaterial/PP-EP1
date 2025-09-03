58556b932e3471f5bfc060a2fdd74f5a96408f9d
thiagopls1 <thiagopls1@hotmail.com>
2025-09-03 13:59:51 -0300

    Add recursive hashmap and change `String` type to `&str`

--- rust-db/src/main.rs
+++ rust-db/src/main.rs
@@ -22,10 +22,27 @@ impl Cpf {
 
 // Valores que suportamos no banco
 #[derive(Debug)]
-enum DBTypes {
-    String(String),
+enum DBTypes<'a> {
+    String(&'a str),
     Int(usize),
     Cpf(Cpf),
+    HashMap(Box<HashMap<&'a str, DBTypes<'a>>>),
+    Boolean(bool),
+}
+
+// O Box é um smart pointer, que aloca o HashMap no Heap ao invés da Stack, já que ele pode ser
+// infinito
+
+fn print_hashmap(hash_map: HashMap<&str, DBTypes>) {
+    for (key, value) in hash_map {
+        match value {
+            DBTypes::String(s) => println!("{key}: {s}"),
+            DBTypes::Int(i) => println!("{key}: {i}"),
+            DBTypes::Cpf(cpf) => println!("{key}: {:?}", cpf.to_string()),
+            DBTypes::Boolean(b) => println!("{key}: {b:?}"),
+            DBTypes::HashMap(boxed_map) => print_hashmap(*boxed_map),
+        }
+    }
 }
 
 fn main() {
@@ -33,17 +50,15 @@ fn main() {
     let mut user_data: HashMap<&str, DBTypes> = HashMap::new();
 
     println!("Inserindo um dado...");
-    user_data.insert("name", DBTypes::String(String::from("Jorge")));
+    user_data.insert("name", DBTypes::String("Jorge"));
     user_data.insert("age", DBTypes::Int(32));
     // Como é um Option, precisamos usar o "unwrap"
     user_data.insert("cpf", DBTypes::Cpf(Cpf::new(12345678900).unwrap()));
 
+    let mut user_props: HashMap<&str, DBTypes> = HashMap::new();
+    user_props.insert("account_enabled", DBTypes::Boolean(true));
+    user_data.insert("properties", DBTypes::HashMap(Box::new(user_props)));
+
     println!("Exemplo de dado:");
-    for (key, value) in user_data {
-        match value {
-            DBTypes::String(s) => println!("{key}: {s}"),
-            DBTypes::Int(i) => println!("{key}: {i}"),
-            DBTypes::Cpf(cpf) => println!("{}: {:?}", key, cpf.to_string()),
-        }
-    }
+    print_hashmap(user_data);
 }

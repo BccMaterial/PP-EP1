0dc04e56b555bbcbdc127b3a731531251a9c00d6
thiagopls1 <thiagopls1@hotmail.com>
2025-09-03 11:11:17 -0300

    Add some introduction to `HashMap`

--- rust-db/src/main.rs
+++ rust-db/src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut map: HashMap<&str, u8> = HashMap::new();
+
+    map.insert("id", 1);
+    map.insert("count", 28);
+
+    // O copied transforma o Option<&T> em Option<T>
+    let count = map.get("count").copied().unwrap_or(0);
+
+    println!("{map:?}");
+    println!("Count: {count}");
 }
